993421ae3ac01edf47f9d0b32565d62ecaa7fd51
DuvalM95 <duvalmunoz2@gmail.com>
2026-02-09 20:52:33 -0500

    V4

--- backend/server.py
+++ backend/server.py
@@ -53,6 +53,6 @@ def view_attempts():
     conn.close()
     return jsonify({'attempts': rows})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
